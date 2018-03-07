bdb432612ff3fd552032b5b25c40531d1abf472e
TildeBeta <tildebetabot@gmail.com>
2018-03-07 15:40:47 +0200

    Implement barebones functionality

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
 import re
 
+import asks
 from curious.core.client import Client
 import multio
 
-from credentials import discord
+from credentials import discord, twitter
 
 
 multio.init('curio')
 
 client = Client(discord.token)
-tweet_pattern = re.compile(r'https?://twitter.com/\S+/\d+')
+tweet_pattern = re.compile(r'https?://twitter.com/\S+/(\d+)')
+
+
+def get_tweet(id):
+    return asks.get(
+        'https://api.twitter.com/1.1/statuses/show.json',
+        headers={'Authorization': twitter.token},
+        params={'id': id}
+    )
 
 
 @client.event('message_create')
@@ -17,8 +26,15 @@ async def parse_tweets(ctx, message):
     if message.author.user.bot:
         return
 
-    for tweet in tweet_pattern.findall(message.content):
-        await message.channel.messages.send(tweet)
+    for tweet in tweet_pattern.finditer(message.content):
+        resp = await get_tweet(tweet.group(1))
+        tweet = resp.json()
+
+        # Ignore first images because discord should show it
+        images = tweet.get('extended_entities', {'media': []})['media'][1:]
+
+        for image in images:
+            await message.channel.messages.send(image['media_url_https'])
 
 
 if __name__ == '__main__':
