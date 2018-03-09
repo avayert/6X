e2a8aedcf1998ad0b25c2ebfedd7a92fc4a12e8b
TildeBeta <tildebetabot@gmail.com>
2018-03-09 14:41:08 +0200

    Send tweet being replied to
    Also a small change to how get_tweet works to remove boilerplate.

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import re
 
 import asks
-from curious.core.client import Client
 import multio
+from curious.core.client import Client
 
 from credentials import discord, twitter
 
@@ -12,12 +12,13 @@ client = Client(discord.token)
 tweet_pattern = re.compile(r'https?://twitter.com/\S+/(\d+)')
 
 
-def get_tweet(id):
-    return asks.get(
+async def get_tweet(id):
+    resp = await asks.get(
         'https://api.twitter.com/1.1/statuses/show.json',
         headers={'Authorization': twitter.token},
         params={'id': id}
     )
+    return resp.json()
 
 
 @client.event('message_create')
@@ -26,14 +27,21 @@ async def parse_tweets(ctx, message):
         return
 
     for tweet in tweet_pattern.finditer(message.content):
-        resp = await get_tweet(tweet.group(1))
-        tweet = resp.json()
+        tweet = await get_tweet(tweet.group(1))
 
         # Ignore first images because discord should show it
         images = tweet.get('extended_entities', {'media': []})['media'][1:]
 
         for image in images:
             await message.channel.messages.send(image['media_url_https'])
+
+        # This is awful please ignore it
+        reply = tweet['in_reply_to_status_id']
+        if reply:
+            replied_to = await get_tweet(reply)
+
+            fmt = 'https://twitter.com/{0[user][screen_name]}/status/{0[id]}'
+            await message.channel.messages.send(fmt.format(replied_to))
 
 
 if __name__ == '__main__':
