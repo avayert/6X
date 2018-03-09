4e97d10d0fb66d7368fb4d3f073e816d6eb0940b
TildeBeta <tildebetabot@gmail.com>
2018-03-10 00:40:36 +0200

    Add an update command
    Pulls changes from git so I can update the bot and apply the changes remotely.

--- bot.py
+++ bot.py
@@ -3,15 +3,38 @@ import re
 import asks
 import multio
 from contextlib import suppress
+
+from curio import subprocess
+from curious import Embed
+from curious.commands import CommandsManager, command
 from curious.core.client import Client
+from operator import methodcaller
 
 from credentials import discord, twitter
 
 multio.init('curio')
 
 client = Client(discord.token)
+manager = CommandsManager.with_client(client, command_prefix='t!')
+
 tweet_pattern = re.compile(r'https?://twitter.com/\S+/(\d+)')
 tweet_fmt = 'https://twitter.com/{0[user][screen_name]}/status/{0[id]}'
+
+
+@command()
+async def update(ctx):
+    # TODO maybe allow arguments for better control (using argparse?)
+    process = subprocess.Popen('git pull'.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = map(methodcaller('decode'), await process.communicate())
+
+    # TODO output might be over 2000 characters...?
+    description = '```diff\n{0}\n{1}```'.format(stdout, stderr)
+    # relies on there not being both stdout and stderr output, let's see
+    embed = Embed(colour=0xcc0000 if stderr else 0x4BB543, description=description)
+
+    await ctx.channel.messages.send(embed=embed)
+
+manager.add_command(update)
 
 
 async def get_tweet(id):
