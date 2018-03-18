da3f5ddb105f22962f9c8a2e8da26757657d58e9
TildeBeta <tildebetabot@gmail.com>
2018-03-18 13:09:40 +0200

    Parse mobile links

--- twitterimages/bot.py
+++ twitterimages/bot.py
@@ -13,7 +13,7 @@ multio.init('curio')
 client = Client(discord.token)
 manager = CommandsManager.with_client(client, command_prefix='t!')
 
-tweet_pattern = re.compile(r'https?://twitter\.com/\S+/(\d+)')
+tweet_pattern = re.compile(r'https?://(?:mobile\.)?twitter\.com/\S+/(\d+)')
 tweet_fmt = 'https://twitter.com/{0[user][screen_name]}/status/{0[id]}'
 
 
