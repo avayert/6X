ca77b02f8dbbecdb5f3504448de6946c919c7a8a
TildeBeta <tildebetabot@gmail.com>
2018-04-15 22:36:11 +0300

    Add logging

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import logging
+
 import curio
 
 from twitterimages.bot import main
 
 if __name__ == '__main__':
+    handler = logging.FileHandler(filename='twitterimages.log', encoding='utf-8', mode='w')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
+        handlers=[handler]
+    )
     curio.run(main)
