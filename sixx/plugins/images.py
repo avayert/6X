be0aa51906e4b0c02ffb93a7a6d7274d0966714b
TildeBeta <tildebetabot@gmail.com>
2018-05-25 19:47:54 +0300

    Almost working VCR command

--- sixx/plugins/images.py
+++ sixx/plugins/images.py
@@ -1,12 +1,14 @@
 import asks
+import datetime
 import numpy as np
+import random
 from PIL import Image
 from PIL.ImageEnhance import Brightness
 from PIL.ImageFont import truetype
 from curious.commands import Context, Plugin, command
 from io import BytesIO
 
-from sixx.plugins.utils.pillow import add_noise, add_scanlines, save_image
+from sixx.plugins.utils.pillow import add_noise, add_scanlines, antialiased_text, save_image
 
 SCANLINES, NOISE, BOTH = range(3)
 
@@ -17,7 +19,7 @@ class Images(Plugin):
     """
 
     @command()
-    async def gett(self, ctx: Context, *, url: str):
+    async def vcr(self, ctx: Context, *, url: str):
         # TODO support attachments
         buffer = BytesIO()
         resp = await asks.get(url, stream=True)
@@ -37,6 +39,27 @@ class Images(Plugin):
                 image = add_noise(image)
 
             Brightness(image).enhance(2.5)
+
+            # hoo boy
+
+            text = np.random.choice(['PLAY', '  PAUSE'], p=[0, 1])
+            font = truetype('VCR_OSD_MONO.ttf', size=int(min(image.size) * 1.2))
+
+            start = datetime.datetime(1980, 1, 1, 0, 0)
+            now = datetime.datetime.utcnow()
+
+            # https://stackoverflow.com/a/8170651/7581432
+            random_date = start + datetime.timedelta(seconds=random.randint(0, int((now - start).total_seconds())))
+
+            topleft_text = antialiased_text(text, font, image.width, image.height, offset_x=1 / 35, offset_y=1 / 15)
+            image.paste(topleft_text, (0, 0), mask=topleft_text)
+
+            # This is a nasty hack but oh well
+            time, date = random_date.strftime('%H:%M|%b. %d %Y').split('|')
+            wrap_width = len(date)
+            botleft_text = antialiased_text(time.ljust(wrap_width) + date, font, image.width, image.height,
+                                            offset_x=1 / 35, offset_y=13 / 15, wrap_width=wrap_width)
+            image.paste(botleft_text, (0, 0), mask=botleft_text)
 
             buffer = save_image(image, format=image.format)
             await ctx.channel.messages.upload(buffer, filename='shoutouts.' + image.format)
