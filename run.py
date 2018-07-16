1ea1432160663dbf3c2b166aa75e87985536500e
TildeBeta <tildebetabot@gmail.com>
2018-07-16 20:14:23 +0300

    Improve logging

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-import logging
+import sys
 
 import curio
+import logging
 
 from sixx.bot import main
 
 if __name__ == '__main__':
-    handler = logging.FileHandler(filename='sixx.log', encoding='utf-8', mode='w')
+    handlers = [logging.FileHandler(filename='sixx.log', encoding='utf-8', mode='w')]
+
+    level = logging.WARNING
+
+    if any(arg == '--debug' for arg in sys.argv):
+        handlers.append(logging.StreamHandler(sys.stdout))
+        level = logging.DEBUG
+
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=level,
         format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
-        handlers=[handler]
+        handlers=handlers
     )
     curio.run(main)
